a320be4cc0be83f569975393cce37625303ead2c
Kevin Phan <ken.phan@live.ca>
2023-04-09 05:12:07 +0700

    Docker images setup for client and server

--- client/index.js
+++ client/index.js
@@ -7,3 +7,9 @@ import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+AppRegistry.runApplication(appName, {
+  initialProps: {},
+  rootTag: document.getElementById('root'),
+  port: 3000,
+});
